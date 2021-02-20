1fcc61d29a92f686c4d7389bd0837bd1ab9c2731
PratikDey <56970651+PratikDey@users.noreply.github.com>
2021-02-20 11:30:29 +0530

    started with the client side

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,33 @@
 const { ApolloServer } = require("apollo-server"); //apollo server er instance
 const typeDefs = require("./schema"); //schema ta amra server e pass korbo ei typeDefs constant er maddhome(etai science)
+const isEmail = require("isemail");
+const { createStore } = require("./utils");
+const resolvers = require("./resolvers");
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
+
+const server = new ApolloServer({
+  context: async ({ req }) => {
+    const auth = (req.headers && req.headers.authorization) || "";
+    const email = Buffer.from(auth, "base64").toString("ascii");
+
+    if (!isEmail.validate(email)) return { user: null };
+
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] ? users[0] : null;
+
+    return { user };
+  },
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(() => {
   console.log("Server is running on port 4000");
